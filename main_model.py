878778d4759d71ca4b9556e3e2430758c746a7c5
akkkb <97660948+akkkb@users.noreply.github.com>
2022-01-13 17:21:03 +0530

    Update main_model.py

--- main_model.py
+++ main_model.py
@@ -5,148 +5,77 @@ from language_model import WordEmbedding, QuestionEmbedding, WordEmbeddingNew
 from classifier import SimpleClassifier,distLinear
 from fc import FCNet
 
-class BaseModel(nn.Module):
-    def __init__(self, w_emb, q_emb,  q_net, v_net, classifier):
-        super(BaseModel, self).__init__()
-        self.w_emb = w_emb
-        self.q_emb = q_emb
-        self.q_net = q_net
-        self.v_net = v_net
-        self.classifier = classifier
-
-    def forward(self, v, q, labels):
-        """Forward
-
-        v: [batch, num_objs, obj_dim]
-        b: [batch, num_objs, b_dim]
-        q: [batch_size, seq_length]
-
-        return: logits, not probs
-        """
-        w_emb = self.w_emb(q)
-        q_emb = self.q_emb(w_emb) # [batch, q_dim]
-        v_emb =  v # [batch, v_dim]
-
-        q_repr = self.q_net(q_emb)
-        v_repr = self.v_net(v_emb)
-        joint_repr = q_repr * v_repr
-        #print(joint_repr.shape)
-        #logits = self.classifier(joint_repr)
-        return joint_repr
-
-class BaseModel_jr(nn.Module):
-    #def __init__(self, w_emb, q_emb,  q_net, v_net):
+class ImgAtt(nn.Module):
     def __init__(self, w_emb, q_emb, v_att, q_net, v_net):
-
-        super(BaseModel_jr, self).__init__()
+        super(ImgAtt, self).__init__()
         self.w_emb = w_emb
         self.q_emb = q_emb
-        self.v_att = v_att
         self.q_net = q_net
         self.v_net = v_net
-
+        self.v_att = v_att
+        
     def forward(self, v, q, labels):
-        """Forward
-        v: [batch, num_objs, obj_dim]
-        b: [batch, num_objs, b_dim]
-        q: [batch_size, seq_length]
-
-        return: logits, not probs
-        """
         w_emb = self.w_emb(q)
         q_emb = self.q_emb(w_emb) # [batch, q_dim]
-        #v_emb =  v # [batch, v_dim]
-        att = self.v_att(v, q_emb)
-        v_emb = (att * v).sum(1)
+        att_v = self.v_att(v, q_emb)
+        v_emb = (att * v).sum(1) # [batch, v_dim]
         q_repr = self.q_net(q_emb)
         v_repr = self.v_net(v_emb)
-        joint_repr = q_repr * v_repr
-##        return v_repr, joint_repr
-        return v_repr, att, joint_repr
+        return v_repr, att_v
 
-class BaseModel_ques(nn.Module):
-    def __init__(self, w_emb, q_emb, net, classifier):
-        super(BaseModel_ques, self).__init__()
-        self.classifier = classifier
-        self.net = net
+class QuesAtt(nn.Module):
+    def __init__(self, w_emb, q_emb, q_att, q_net):
+        super(QuesAtt, self).__init__()
         self.w_emb = w_emb
         self.q_emb = q_emb
-
-    def forward(self, q, labels):
-        """Forward
-        v: [batch, num_objs, obj_dim]
-        b: [batch, num_objs, b_dim]
-        q: [batch_size, seq_length]
-        return: logits, not probs
-        """
-#        w_emb = self.w_emb(q)
-#        q_emb = self.q_emb(w_emb) # [batch, q_dim]
-#        q_repr = self.net(q_emb)
-#        q_proj = self.net(q)
-        #print(q_proj.shape)
-        logits = self.classifier(q)
-        #print("Representation of question is:",q_repr.shape) 
-        return logits
-
-class BaseModel_rev(nn.Module):
-    def __init__(self, w_emb, q_emb, v_att, q_net, classifier):
-        super(BaseModel_rev, self).__init__()
-        self.w_emb = w_emb
-        self.q_emb = q_emb
-        self.v_att = v_att
+        self.q_att = q_att
         self.q_net = q_net
-        # self.v_net = v_net
-        self.classifier = classifier
 
     def forward(self, v, q, labels):
-        """Forward
-        v: [batch, num_objs, obj_dim]
-        b: [batch, num_objs, b_dim]
-        q: [batch_size, seq_length]
-        return: logits, not probs
-        """
         w_emb = self.w_emb(q)
-#         q_emb = self.q_emb(w_emb) # [batch, q_dim]
-
-        att = self.v_att(w_emb,v)
+        att_q = self.q_att(w_emb, v)
         w = (att * w_emb) # [batch, v_dim]
         q_emb = self.q_emb(w) 
         q_repr = self.q_net(q_emb)
-#         v_repr = self.v_net(v_emb)
-#         joint_repr = q_repr * v_repr
-#         logits = self.classifier(joint_repr)
-        return q_repr,att
+        return q_repr, att_q
 
-def joint_emb(dataset, num_ans_candidates, num_hid):
-    w_emb = WordEmbedding(dataset, 300, 0.0)
-    q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
-    v_att = NewAttention(num_hid, q_emb.num_hid, num_hid)
-    q_net = FCNet([num_hid, num_hid])
-    v_net = FCNet([num_hid, num_hid])
-    return BaseModel_jr(w_emb, q_emb, v_att, q_net, v_net)
+class QuesCat(nn.Module):
+    def __init__(self, classifier):
+        super(QuesCat, self).__init__()
+        self.classifier = classifier
 
-def ques_cat(dataset, num_ans_candidates, num_hid):
-    w_emb = WordEmbedding(dataset, 300, 0.0)
-    q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
-    q_net = FCNet([q_emb.num_hid, num_hid])
-    net = FCNet([num_hid, num_hid])
+    def forward(self, q, labels):
+        logits = self.classifier(q)
+        return logits
+
+class AnsPred(nn.Module):
+    def __init__(self, classifier):
+        super(AnsPred, self).__init__()
+        self.classifier = classifier
+
+    def forward(self, emb):
+        ans = self.classifier(emb)
+
+def ques_cat(dataset, num_hid):
     classifier = SimpleClassifier(num_hid, int(num_hid / 2), 12, 0.5)
-    return BaseModel_ques(w_emb, q_emb, net, classifier)
+    return QuesCat(w_emb, q_emb, net, classifier)
 
-def image_att(dataset, num_ans_candidates, num_hid):
+def image_att(dataset, num_hid):
     w_emb = WordEmbedding(dataset, 300, 0.0)
     q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
-    #v_att = NewAttention(dataset.v_dim, q_emb.num_hid, num_hid)
+    v_att = NewAttention(dataset.v_dim, q_emb.num_hid, num_hid)
     q_net = FCNet([q_emb.num_hid, num_hid])
     v_net = FCNet([num_hid, num_hid])
-    classifier = SimpleClassifier(num_hid, num_hid * 2, num_ans_candidates, 0.5)
-    return BaseModel(w_emb, q_emb, q_net, v_net, classifier)
+    return ImgAtt(w_emb, q_emb, v_att, q_net, v_net)
 
-def ques_att(dataset, num_ans_candidates, num_hid):
+def ques_att(dataset, num_hid):
     w_emb = WordEmbedding(dataset, 300, 0.0)
     q_emb = QuestionEmbedding(300, num_hid, 1, False, 0.0)
     v_att = NewAttention_rev(300, q_emb.num_hid, num_hid)
     q_net = FCNet([q_emb.num_hid, num_hid])
     v_net = FCNet([num_hid, num_hid])
-    classifier = SimpleClassifier(num_hid, num_hid * 2, num_ans_candidates, 0.5)
-    return BaseModel_rev(w_emb, q_emb, v_att, q_net, classifier)
+    return QuesAtt(w_emb, q_emb, v_att, q_net)
+
+def build_qus(dataset, num_ans_candidates, num_hid):
+    classifier = SimpleClassifier(num_hid, num_hid, num_ans_candidates, 0.5)
+    return AnsPred(classifier)
