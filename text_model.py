fa1da6687ea5e1fa8be2118a68da0a8e3bafef6c
akkkb <97660948+akkkb@users.noreply.github.com>
2022-01-13 21:30:49 +0530

    Update text_model.py

--- text_model.py
+++ text_model.py
@@ -3,45 +3,6 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 
-class WordEmbeddingNew(nn.Module):
-    """Word Embedding
-
-    The ntoken-th dim is used for padding_idx, which agrees *implicitly*
-    with the definition in Dictionary.
-    """
-    def __init__(self, ntoken, emb_dim, dropout, op=''):
-        super(WordEmbeddingNew, self).__init__()
-        self.op = op
-        self.emb = nn.Embedding(ntoken+1, emb_dim, padding_idx=ntoken)
-        if 'c' in op:
-            self.emb_ = nn.Embedding(ntoken+1, emb_dim, padding_idx=ntoken)
-            self.emb_.weight.requires_grad = False # fixed
-        self.dropout = nn.Dropout(dropout)
-        self.ntoken = ntoken
-        self.emb_dim = emb_dim
-
-    def init_embedding(self, np_file, tfidf=None, tfidf_weights=None):
-        weight_init = torch.from_numpy(np.load(np_file))
-        assert weight_init.shape == (self.ntoken, self.emb_dim)
-        weight_init_clone = torch.zeros(weight_init.shape)
-        self.emb.weight.data[:self.ntoken] = weight_init
-        if tfidf is not None:
-            if 0 < tfidf_weights.size:
-                weight_init = torch.cat([weight_init, torch.from_numpy(tfidf_weights)], 0)
-            weight_init = tfidf.matmul(weight_init) # (N x N') x (N', F)
-            self.emb_.weight.requires_grad = True
-        if 'c' in self.op:
-            weight_init_clone[:weight_init.size(0)] = weight_init.clone()
-            self.emb_.weight.data[:self.ntoken] = weight_init_clone#weight_init.clone()
-
-    def forward(self, x):
-        emb = self.emb(x)
-        if 'c' in self.op:
-            emb = torch.cat((emb, self.emb_(x)), 2)
-        emb = self.dropout(emb)
-        return emb
-
-
 class WordEmbedding(nn.Module):
     """Word Embedding
 
